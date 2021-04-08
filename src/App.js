8ebbbeb113f1c55f94e0be6fe59a63ddef7bc5f5
jackrc7 <cardin.j@husky.neu.edu>
2021-04-08 15:27:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Search from './search';
+import { useState } from 'react';
+
+const posts = [
+    { id: '1', name: 'apple' },
+    { id: '2', name: 'samsung' },
+    { id: '3', name: 'bose' },
+    { id: '4', name: 'trustmapp' },
+    { id: '5', name: 'Tjx' },
+    { id: '6', name: 'Covestone' },
+    { id: '7', name: 'BLAZENEU' },
+];
+
+const filterPosts = (posts, query) => {
+    if (!query) {
+        return posts;
+    }
+
+    return posts.filter((post) => {
+        const postName = post.name.toLowerCase();
+        return postName.includes(query);
+    });
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('s');
+    const [searchQuery, setSearchQuery] = useState(query || '');
+    const filteredPosts = filterPosts(posts, searchQuery);
+
+    return ( <div >
+        <Search searchQuery = { searchQuery }
+        setSearchQuery = { setSearchQuery }/> 
+        <ul> {
+            filteredPosts.map(post => ( <li key = { post.key } > { post.name } </li>
+            ))
+        } </ul> 
+        
+        </div>
+    );
 }
+
 
 export default App;
